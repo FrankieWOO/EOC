76051da62fad7f9bba1a0c0d711ca70c9f3cb15a
Fan <k1506456@kcl.ac.uk>
2017-06-14 11:38:22 +0100

    1
    1

---
+++ sketchbook/maccepavd_mega/maccepavd_mega.ino
@@ -0,0 +1,234 @@
+/* control a single servo by usec on arduino uno
+ * implementing writemicrosend()
+ * for arduino mega2560. should be compatible with uno
+ * if no low level code and functions are used.
+ * pub ROS topics: sensors_raw;
+ * 
+ */
+#include <avr/sleep.h>
+#include <ros.h>
+#include <maccepavd/SensorsRaw.h>
+#include <maccepavd/CommandRaw.h>
+#include <std_msgs/UInt16.h>
+#include <ros/time.h>
+//#include <Servo.h>
+
+//#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+//#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+int pin_servo1 = 11;
+int pin_servo2 = 12;
+int pin_jointsensor = 0;
+int pin_servo1sensor = 1;
+int pin_servo2sensor = 2;
+#define pin_damping 5
+float Vcc;
+
+float joint_read;
+float servo1_read;
+float servo2_read;
+int sensor_counter =0;
+
+const int numReadings = 4;
+int jnt_reads[numReadings];
+
+ros::NodeHandle nh;
+maccepavd::SensorsRaw sensors_msg;
+
+union{
+  struct{
+    unsigned int u1; // servo command of servo1
+    unsigned int u2;
+    unsigned int u3;
+  };
+  unsigned int values[2];
+}command_buffer;
+
+void command_cb(const maccepavd::CommandRaw& cmd_msg){
+  command_buffer.u1 = cmd_msg.u1;
+  command_buffer.u2 = cmd_msg.u2;
+  command_buffer.u3 = cmd_msg.u3;
+  //servo1.writeMicroseconds(cmd_msg.u1);
+  //servo2.writeMicroseconds(cmd_msg.u2);
+  analogWrite(pin_damping,cmd_msg.u3);
+}
+
+ros::Subscriber<maccepavd::CommandRaw> sub("command_raw",command_cb);
+ros::Publisher sensors_raw("sensors_raw",&sensors_msg);
+
+void setup_timers(){
+  uint8_t oldSREG = SREG;
+  cli();
+  // fast PWM mode (mode14), WGM13-10:1 1 1 0
+  // Compare Output mode: non-inverting mode. COM1A1-0: 1 0
+  TCCR1A = 1<<WGM11 | 1<<COM1A1 & ~_BV(COM1A0) | 1<<COM1B1 & ~_BV(COM1B0) ;
+  TCCR1B = 1<<WGM13 | 1<<WGM12 | 1<<CS11;
+  // setup ICR1 at which the clock resets
+  // ATmega2560 clock speed: 16MHz, prescaler: 8
+  // targeted PWM frequency: 50 Hz
+  // ICR1 = F_cpu / (PreScaler*F_pwm) - 1
+  ICR1 = 40000 - 1;
+  
+  OCR1A = command_buffer.u1;
+  OCR1B = command_buffer.u2;
+  
+  // enable timer overflow interrupt
+  
+  TIMSK1 &=  ~_BV(OCIE1A);   // Disable Timer 1 Compare Match A (OCIE1A) interrupt (not used) 
+  TIMSK1 &=  ~_BV(OCIE1B);   // Disable Timer 1 Compare Match B (OCIE1B) interrupt (not used) 
+  TIMSK1 |= 1<<TOIE1;
+  
+  // setup timer2
+  //
+  //TCCR2A = 0; // WGM22:0 := normal mode
+  //TCCR2B = ( 1 << CS22 ) | (1 << CS21 ) ; //prescaler 128: 101; 256: 110; 64:100
+  //TCNT2 = 0;
+  //
+  //-------------------//
+  //ASSR   &= ~_BV(AS2); //internal I/O clock
+  
+  //TCCR2A  = 0;               // Set Timer 2 to normal mode
+  //TCCR2B |= _BV(CS02);      // Set prescaler for Timer 2. 
+  //TCCR2B |= _BV(CS01);
+  //-------------------//
+  //TIMSK2 |=  _BV(TOIE2);     // Enable Timer 2 Overflow (TOIE2) interrupt
+  //TIMSK2 &= ~_BV(OCIE2A);    // Disable Compare Match A (OCIE2A) interrupt (not used)
+  
+    
+  
+  SREG = oldSREG;
+}
+
+ISR(TIMER1_OVF_vect){
+  OCR1A = command_buffer.u1*2;
+  OCR1B = command_buffer.u2*2;
+  //sensors_msg.servo1_sensor = servo1_read*Vcc/1023.0;
+  
+  
+}
+/*
+ISR(TIMER2_OVF_vect){
+  //joint_read = joint_read + analogRead(pin_jointsensor);
+  //servo1_read = servo1_read + analogRead(pin_servo1sensor);
+  //servo1_read += float(analogRead(pin_servo1sensor));
+  //servo2_read = servo2_read + analogRead(pin_servo2sensor);
+  //sensor_counter += 1;
+}
+*/
+void setupADC(){
+  ADCSRA &= ~_BV( ADPS2 );
+  ADCSRA |= _BV(ADPS1);
+  ADCSRA |= _BV(ADPS0);
+  
+}
+
+void setup() {
+  //Serial.begin(57600);
+  //setupADC();
+  pinMode(pin_servo1,OUTPUT);
+  pinMode(pin_servo2,OUTPUT);
+  pinMode(pin_damping,OUTPUT);
+  
+  Vcc = readVcc()/1000.0;
+  joint_read = float(analogRead(pin_jointsensor));
+  servo1_read= float(analogRead(pin_servo1sensor));
+  servo2_read= float(analogRead(pin_servo2sensor));
+  command_buffer.u1 = 1500;
+  command_buffer.u2 = 900;
+  setup_timers();
+  //servo1.attach(pin_servo1);
+  //servo2.attach(pin_servo2);
+  //servo1.writeMicroseconds(1500);
+  //servo2.writeMicroseconds(900);
+  
+  delay(10);
+  
+  nh.initNode();
+  //nh.advertise(sensor1);
+  nh.advertise(sensors_raw);
+  nh.subscribe(sub);
+}
+
+void loop() {
+  //transmit_index = 0;
+  //UCSR0B |= _BV(TXCIE0);
+  //unsigned long t1 = micros();
+
+  joint_read = analogRead(pin_jointsensor);
+  servo1_read = analogRead(pin_servo1sensor);
+  //servo1_read += float(analogRead(pin_servo1sensor));
+  servo2_read = analogRead(pin_servo2sensor);
+  //sensor_counter += 1;
+  //delay(1);
+  //if(sensor_counter == 4){
+  
+  
+  sendmsg();
+  //servo1_read = 0;
+  //servo2_read = 0;
+  //joint_read = 0;
+  //TCNT2 = 0;
+  //sensor_counter = 0;
+  //}
+  nh.spinOnce();
+}
+
+void sendmsg(){
+  sensors_msg.header.stamp = nh.now() ;
+  sensors_msg.joint_sensor = joint_read*Vcc/1023.0;
+  sensors_msg.servo1_sensor = servo1_read*Vcc/1023.0;
+  
+  sensors_msg.servo2_sensor = servo2_read*Vcc/1023.0;
+  
+  sensors_raw.publish(&sensors_msg);
+  
+}
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+  #if defined(__AVR_ATmega2560__)
+    ADCSRB &= ~_BV(MUX5); // Without this the function always returns -1 on the ATmega2560
+  #endif
+  delay(20); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
+int analogNoiseReducedRead(int pinNumber)
+{
+ int reading;
+ 
+ ADCSRA |= _BV( ADIE );             //Set ADC interrupt
+ set_sleep_mode(SLEEP_MODE_ADC);    //Set sleep mode
+ reading = analogRead(pinNumber);   //Start reading
+ sleep_enable();                    //Enable sleep
+ do
+ {                                  //Loop until reading is completed
+   sei();                           //Enable interrupts
+   sleep_mode();                    //Go to sleep
+   cli();                           //Disable interrupts
+ } while(((ADCSRA&(1<<ADSC))!= 0)); //Loop if the interrupt that woke the cpu was something other than the ADC finishing the reading
+ sleep_disable();                   //Disable sleep
+ ADCSRA &= ~ _BV( ADIE );           //Clear ADC interupt
+ sei();                             //Enable interrupts
+ 
+ return(reading);
+}
