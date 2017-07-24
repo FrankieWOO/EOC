c7520cfb32acafb98fb8f05d936f3aa66e740190
Fan <k1506456@kcl.ac.uk>
2017-07-24 18:55:35 +0100

    1
    1

--- matlab/robot_models/actuators/IdealVIA.m
+++ matlab/robot_models/actuators/IdealVIA.m
@@ -8,10 +8,14 @@ classdef IdealVIA
         %gear_d = 20;
         %Kd = 0.0212;
         %Rd = 21.2;
-        ratio_load = 1;
+        ratio_load = 1; % := R_l/R_m
         Rl
         %max_damping_db
+        max_rege_damping
         max_damping
+        u_max_regedamp
+        
+        max_stiffness
         
         umin  
         umax 
@@ -23,16 +27,26 @@ classdef IdealVIA
                 p = varargin{1};
                 if isfield(p, 'umin'), obj.umin = p.umin; end
                 if isfield(p, 'umax'), obj.umax = p.umax; end
-                if isfield(p, 'ratio_load'), obj.ratio_load=p.ratio_load;end
+                if isfield(p, 'ratio_load'), obj.ratio_load = p.ratio_load;end
+                if isfield(p, 'max_damping', obj.max_damping = p.max_damping;end
+                if isfield(p, 'max_stiffness'), obj.max_stiffness=p.max_stiffness;end
             end
+            
+            obj.max_rege_damping = obj.max_damping/(1 + obj.ratio_load);
+            obj.u_max_regedamp = 1/(1 + obj.ratio_load);
+            
         end
         
         function tau = torque(obj, q, qdot, u1, u2, u3)
-            tau = u2*(u1 - q) - u3*qdot;
+            k = obj.stiffness(u2);
+            d = obj.damping(u3);
+            tau = k*(u1 - q) - d*qdot;
         end
-        
+        function k = stiffness(obj, u2)
+            k = u2*obj.max_stiffness;
+        end
         function d = damping(obj, u3)
-            d = u3;
+            d = u3*obj.max_damping;
         end
         
         function tau_d = torque_damp(obj, qdot, u3)
@@ -40,7 +54,7 @@ classdef IdealVIA
         end
         
         function power = power_rege(obj, qdot, u3)
-            power = obj.torque_damp(qdot, u3)*qdot*obj.ratio_load/(1+obj.ratio_load);
+            
         end
     end
     
